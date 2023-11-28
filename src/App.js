1956ce2903bc283d9144fb899f991474bbf6f030
visalithota <thotavisali@gmail.com>
2023-11-28 14:28:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,91 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import"./App.css"
+ import { useState,useEffect,useRef } from "react";
+ const App =()=>{
+    const [count,setCount]=useState(0)
+    const [state,setState]=useState(false)
+    const buttonElement = useRef(null);
+ useEffect(()=>{
+    let counter=""
+    if(state){
+        counter =setInterval(()=>{ 
+            setCount((prevCount)=>prevCount+1)},500)
+            
+    }
+     return()=>clearInterval(counter)
+  },[state])
+  
+  const startButton=()=>{
+   
+    buttonElement.current.disabled=true
 
-function App() {
-  const [popularMovies, setPopularMovies] = useState([]);
-  const [movieName, setMovieName] = useState('');
-  const [searchedMovies, setSearchedMovies] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get('https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716')
-      .then((res) => {
-        setPopularMovies(res.data.results);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+    setState(true)
+  }
+  const stopButton=()=>{
+    buttonElement.current.disabled=false
+    setState(false)
+  }
 
-  useEffect(() => {
-    axios
-      .get(`https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=cfe422613b250f702980a3bbf9e90716`)
-      .then((res) => {
-        setSearchedMovies(res.data.results);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [movieName]);
-
-  return (
-    <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Movie Search Engine</h1>
-        <input
-          value={movieName}
-          onChange={(e) => {
-            setMovieName(e.target.value);
-          }}
-          placeholder="Enter movie name"
-        />
-      </div>
-      <div id="movies-wrapper">
-        {movieName === ''
-          ? popularMovies.map((item, i) => {
-              return (
-                <div className="movie-card" key={item.id}>
-                  <img
-                    className="movie-thumbnail"
-                    src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${item.poster_path}`}
-                    alt={item.title}
-                  />
-                  <p>
-                    <b>Title :</b> {item.title}
-                  </p>
-                  <p>
-                    <b>Rating :</b> {item.vote_average} / 10{' '}
-                  </p>
-                  <p>
-                    <b>Release Date :</b> {item.release_date}
-                  </p>
-                </div>
-              );
-            })
-          : searchedMovies.map((item, i) => {
-              return (
-                <div className="movie-card" key={item.id}>
-                  <img
-                    className="movie-thumbnail"
-                    src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${item.poster_path}`}
-                    alt={item.title}
-                  />
-                  <p>
-                    <b>Title :</b> {item.title}
-                  </p>
-                  <p>
-                    <b>Rating :</b> {item.vote_average} / 10{' '}
-                  </p>
-                  <p>
-                    <b>Release Date :</b> {item.release_date}
-                  </p>
-                </div>
-              );
-            })}
-      </div>
-    </>
-  );
-}
-
-export default App;
+    return(
+        <>
+        <p>Count:{count}</p>
+        <button id="btn" ref={buttonElement} onClick={()=>{startButton()}}>Start</button>
+        <button  onClick={()=>{stopButton()}}>Stop</button>
+        </>
+    )
+   
+ } 
+ export default App;
